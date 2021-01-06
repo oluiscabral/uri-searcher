713868b4e995f38264bbee49d58346858656480e
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2021-01-06 18:08:04 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+author: @oluiscabral
+
+This code has the objective to search names of either universities
+and people profile in the platform https://www.urionlinejudge.com.br/
+
+The idea is to provide the URL of a list on this site,
+then the program will return the searched profile if it exists
+"""
