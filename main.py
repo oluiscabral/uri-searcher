eeddeee64c65f3e2cda4767066e23e1b66e5256d
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2021-01-06 19:58:50 -0300

    Create the template

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ and people profile in the platform https://www.urionlinejudge.com.br/
 The idea is to provide the URL of a list on this site,
 then the program will return the searched profile if it exists
 """
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def search(url: str, search_name: str):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+
+def main():
+    url = input()
+    search_name = input()
+    r = requests.get(url, search_name)
